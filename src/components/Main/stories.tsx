a5847d2c8dc56d5d114b77bcdfe2c6d985f4186f
Guillermo Angulo <anyermo@gmail.com>
2020-09-14 11:01:36 -0300

    Add storybook configuration

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,13 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main
+} as Meta
+
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'React Avançado',
+  description: 'Typescript, ReactJS, NextJS e Styled Components'
+}
